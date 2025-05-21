618737323dfd16d65bc126280315ae07516db28d
cedricsthomasjr <cedricsthomasjr@gmail.com>
2025-05-21 02:53:07 -0400

    disable eslint blocking for production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // disables ESLint blocking production
+  },
 };
 
 export default nextConfig;
